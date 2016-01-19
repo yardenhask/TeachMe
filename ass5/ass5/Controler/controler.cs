51cb94f779206a42fb7c4ffd9b457439b28435d4
yardenhask <yarden.haskal@gmail.com>
2016-01-19 10:53:10 +0200

    controler
    upload

---
+++ ass5/ass5/Controler/controler.cs
@@ -0,0 +1,49 @@
+﻿using ass5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass5.Controler
+{
+    public class controler
+    {
+        public model m;
+        public MainWindow v;
+        //yarden
+
+        public controler() { }
+
+        public void setModel(model model) { m = model; }
+        public void setView(MainWindow view) { v = view; }
+
+        public void addNewUser(string fname, string lname, string id, string phone, string address, string password, string email, bool isTeacher)
+        {
+            m.addUser(fname, lname, id, phone, address, password, email, isTeacher);
+
+        }
+
+        public bool login(string username, string password)
+        {
+            if (m.checkPassword(username, password))
+                return true;
+            return false;
+        }
+
+        public bool addNewAssigment(string path, string user, string teacher)
+        {
+            return (m.addMaterial(path, user, teacher));
+        }
+
+        public List<user> searchTeacher(string city)
+        {
+            return m.searchByCity(city);
+        }
+
+        internal List<user> findStudents(string t)
+        {
+            return m.findAvilableStudents(t);
+        }
+    }
+}
---
+++ ass5/ass5/Controler/user.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass5.Controler
+{
+    public class user
+    {
+        public string fname;
+        public string lname;
+        public string id;
+        public string phone;
+        public string address;
+        public string password;
+        public string email;
+        public char teacher;
+        public user(string fname1, string lname1, string id1, string phone1, string address1, string password1, string email1, bool isTeacher1)
+        {
+            fname = fname1;
+            lname = lname1;
+            id = id1;
+            phone = phone1;
+            address = address1;
+            password = password1;
+            email = email1;
+            if (isTeacher1)
+                teacher = 't';
+            else
+                teacher = 's';
+        }
+
+    }
+}
